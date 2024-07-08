22f85aa1269036715baf6efd255431ff1f1e0819
João Vitor Aires Teixeira <130006397+jvtarss@users.noreply.github.com>
2024-07-08 19:43:24 -0300

    Update and rename dif.R to plots.R

--- plots.R
+++ plots.R
@@ -1,37 +1,28 @@
 #!/usr/bin/env Rscript
 
-# Carregar as bibliotecas necessárias
 library(DESeq2)
 library(ggplot2)
 library(pheatmap)
 
-# Ler os dados de contagem
 countData <- read.table("/home/jvtars/ipe/quantification_results/combined_counts.txt", header=TRUE, row.names=1)
 
-# Preparar as informações das amostras
 sampleInfo <- data.frame(
   row.names = colnames(countData),
   condition = c("condition1", "condition1", "condition1", "condition2", "condition2", "condition2")
 )
 
-# Criar o dataset DESeq2
 dds <- DESeqDataSetFromMatrix(countData = countData, colData = sampleInfo, design = ~ condition)
 
-# Executar a análise de expressão diferencial
 dds <- DESeq(dds)
 
-# Obter os resultados
 res <- results(dds)
 
-# Salvar os resultados em um arquivo
 write.csv(as.data.frame(res), file="/home/jvtars/ipe/differential_expression_results.csv")
 
-# Gráfico MA
 png("/home/jvtars/ipe/plots/MA_plot.png")
 plotMA(res, main="MA Plot", ylim=c(-2,2))
 dev.off()
 
-# Gráfico de vulcão
 res$log2FoldChange <- as.numeric(res$log2FoldChange)
 res$pvalue <- as.numeric(res$pvalue)
 res$padj <- as.numeric(res$padj)
@@ -45,7 +36,6 @@ ggplot(res, aes(x=log2FoldChange, y=-log10(pvalue), color=significant)) +
   labs(title="Volcano Plot", x="Log2 Fold Change", y="-Log10 P-value")
 dev.off()
 
-# Heatmap dos 20 genes mais diferencialmente expressos
 top20 <- head(order(res$padj), 20)
 mat <- assay(rlog(dds))[top20,]
 mat <- mat - rowMeans(mat)
