45ceb9066fdc781a12b67f49e91617f70f18d125
João Vitor Aires Teixeira <130006397+jvtarss@users.noreply.github.com>
2024-07-08 19:42:14 -0300

    Update inst.R

--- inst.R
+++ inst.R
@@ -1,17 +1,13 @@
 #!/usr/bin/env Rscript
 
-# Definir um diretório de biblioteca específico no diretório home
 lib_path <- "~/R/x86_64-pc-linux-gnu-library/4.0"
 
-# Criar o diretório de biblioteca se ele não existir
 if (!dir.exists(lib_path)) {
   dir.create(lib_path, recursive = TRUE)
 }
 
-# Definir o diretório de biblioteca
 .libPaths(lib_path)
 
-# Instalar pacotes no diretório de biblioteca especificado
 install.packages("BiocManager", lib = lib_path)
 BiocManager::install("DESeq2", lib = lib_path)
 install.packages("ggplot2", lib = lib_path)
